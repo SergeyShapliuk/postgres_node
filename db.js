6795b712ce539dd9e59e9c5df8f487fffcfae82e
Сергей <sergeshapluk@gmail.com>
2022-10-05 17:54:29 +0300

    fix: port

--- db.js
+++ db.js
@@ -8,9 +8,10 @@ const isProduction = process.env.NODE_ENV === 'production';
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 console.log("env", process.env.DB_PASSWORD)
 const pool = new Pool({
-    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    connectionString: isProduction ? process.env.DATABASE_URL?ssl=true : connectionString,
     ssl: isProduction,
 });
+console.log("rwerwer",process.env.DB_USER)
 pool.on('connect', () => {
     console.log('Teamwork Database connected successfully!');
 });
