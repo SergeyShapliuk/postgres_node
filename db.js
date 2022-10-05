fb94658313a1c84314769ef8e93ab7a866fc86ea
Сергей <sergeshapluk@gmail.com>
2022-10-05 20:17:06 +0300

    feat: add setting postgres

--- db.js
+++ db.js
@@ -5,13 +5,14 @@ dotenv.config()
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
-console.log("env", process.env.DB_PASSWORD)
+const connectionDev = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+const connectionProd = `postgresql://${process.env.PROD_DB_USER}:${process.env.PROD_DB_PASSWORD}@${process.env.PROD_DB_HOST}:${process.env.PROD_DB_PORT}/${process.env.PROD_DB_DATABASE}`;
+
 const pool = new Pool({
-    connectionString: isProduction ? process.env.DATABASE_URL: connectionString,
-    ssl: true,
+    connectionString: isProduction ? connectionProd: connectionDev,
+    ssl: isProduction,
 });
-console.log("rwerwer",process.env.DB_USER)
+console.log("rwerwer",connectionProd)
 pool.on('connect', () => {
     console.log('Teamwork Database connected successfully!');
 });
