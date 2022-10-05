039435eb273bc67efc0bda082daca18d7cb706c1
Сергей <sergeshapluk@gmail.com>
2022-10-05 18:02:42 +0300

    fix: ssl

--- db.js
+++ db.js
@@ -8,8 +8,8 @@ const isProduction = process.env.NODE_ENV === 'production';
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 console.log("env", process.env.DB_PASSWORD)
 const pool = new Pool({
-    connectionString: isProduction ? process.env.DATABASE_URL?ssl=true : connectionString,
-    ssl: isProduction,
+    connectionString: isProduction ? process.env.DATABASE_URL: connectionString,
+    ssl: true,
 });
 console.log("rwerwer",process.env.DB_USER)
 pool.on('connect', () => {
