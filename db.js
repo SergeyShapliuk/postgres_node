4e3f8739d21ad998f4c2d924389342b011eca560
Сергей <sergeshapluk@gmail.com>
2022-10-06 19:55:37 +0300

    fix: db.js(ssl)

--- db.js
+++ db.js
@@ -6,10 +6,12 @@ dotenv.config()
 const isProduction = process.env.NODE_ENV === 'production';
 
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
-console.log("env", process.env.DB_PASSWORD)
+
 const pool = new Pool({
     connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-    ssl: isProduction,
+    ssl: {
+        rejectUnauthorized:false
+    },
 });
 
 pool.on('connect', () => {
